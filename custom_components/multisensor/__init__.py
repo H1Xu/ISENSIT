d69eaf0b88e02a5197d56c0c680905a89e7265ce
H1Xu <124353124+H1Xu@users.noreply.github.com>
2023-04-03 16:00:00 +0800

    Delete custom_components/multisensor directory

--- custom_components/multisensor/__init__.py
+++
@@ -1,6 +0,0 @@
-from homeassistant.core import HomeAssistant
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
-
-from .const import DOMAIN
-from homeasssitant.const import Platform
