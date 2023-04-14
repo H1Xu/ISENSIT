b0ba8650df4f2c6a82071765b5535f90836da8ca
H1Xu <124353124+H1Xu@users.noreply.github.com>
2023-04-14 17:45:00 +0800

    Update config_flow.py

--- custom_components/multi_sensor/config_flow.py
+++ custom_components/multi_sensor/config_flow.py
@@ -1,4 +1,3 @@
-"""Adds config flow for worlds_air_quality_index integration."""
 from __future__ import annotations
 
 from typing import Any
@@ -14,21 +13,22 @@ from .const import (
 )
 
 
-class ISENSITFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+class ISENSITFlowHandler(config_entries.ConfigFlow):
     #Handle a config flow for worlds_air_quality_index integration.
 
     VERSION = 1
 
-async def async_step_user(self, user_input = None):
+    async def async_step_user(self, user_input = None):
         #Handle the initial step of user input.
+        errors = {}
 
         if user_input is None:
-        #verify input of users
-
+            #verify input of users
             return await self.async_step_sensor_id()
             #if input then call step staion id function
         
         return self.async_show_form(
+            title = localized(self.hass, "componenet.multi_sensor.title"),
             step_id = "user",
             data_schema = vol.Schema(
                 {
@@ -37,13 +37,11 @@ async def async_step_user(self, user_input = None):
                 }
             ),
         )
-        
-
-
 
     async def async_step_sensor_id(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(
+                title = "sensor ID",
                 data = {},
             )
         
