87e6fdac680e7149b9db1c74afbfa5e173aeab14
H1Xu <124353124+H1Xu@users.noreply.github.com>
2023-04-14 16:38:30 +0800

    Add files via upload

---
+++ custom_components/multi_sensor/config_flow.py
@@ -0,0 +1,57 @@
+"""Adds config flow for worlds_air_quality_index integration."""
+from __future__ import annotations
+
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant import config_entries
+
+from homeassistant.helpers.translation import localized
+
+from .const import (
+    DOMAIN,
+)
+
+
+class ISENSITFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+    #Handle a config flow for worlds_air_quality_index integration.
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input = None):
+        #Handle the initial step of user input.
+
+        if user_input is None:
+        #verify input of users
+
+            return await self.async_step_sensor_id()
+            #if input then call step staion id function
+        
+        return self.async_show_form(
+            title = localized(self.hass, "componenet.multi_sensor.title"),
+            step_id = "user",
+            data_schema = vol.Schema(
+                {
+                    vol.Required("sensor_id"): str,
+                    #vol method requires station id to be string
+                }
+            ),
+        )
+        
+
+
+
+    async def async_step_sensor_id(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(
+                title = "sensor ID",
+                data = {},
+            )
+        
+        return self.async_show_form(
+            step_id = "sensor_id",
+            data_schema = vol.Schema({
+                vol.Required("sensor_id"):str
+            }),
+        )
