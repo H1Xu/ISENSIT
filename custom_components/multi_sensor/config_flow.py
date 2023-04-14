62f38ee551ce20c148ee5824307ee78cb57ec516
H1Xu <124353124+H1Xu@users.noreply.github.com>
2023-04-14 17:16:46 +0800

    Update config_flow.py

--- custom_components/multi_sensor/config_flow.py
+++ custom_components/multi_sensor/config_flow.py
@@ -1,4 +1,3 @@
-"""Adds config flow for worlds_air_quality_index integration."""
 from __future__ import annotations
 
 from typing import Any
@@ -9,50 +8,49 @@ from homeassistant import config_entries
 
 from homeassistant.helpers.translation import localized
 
-from .const import (
-    DOMAIN,
-)
+from .const import DOMAIN
 
 
-class WorldsAirQualityIndexConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    #Handle a config flow for worlds_air_quality_index integration.
+class ISENSITFlowHandler(config_entries.ConfigFlow):
+    # Handle a config flow for worlds_air_quality_index integration.
 
     VERSION = 1
 
-    async def async_step_user(self, user_input = None):
-        #Handle the initial step of user input.
+    async def async_step_user(self, user_input=None):
+        # Handle the initial step of user input.
         errors = {}
 
-        if user_input is None:
-        #verify input of users
+        if user_input is not None:
+            return self.async_show_form(
+                title=localized(self.hass, "component.multi_sensor.title"),
+                step_id="user",
+                data_schema=vol.Schema(
+                    {
+                        vol.Required("sensor_id"): str,
+                    }
+                ),
+                errors=errors,
+            )
 
-            return await self.async_step_sensor_id()
-            #if input then call step staion id function
-        
         return self.async_show_form(
-            title = localized(self.hass, "componenet.multi_sensor.title"),
-            step_id = "user",
-            data_schema = vol.Schema(
+            title=localized(self.hass, "component.multi_sensor.title"),
+            step_id="user",
+            data_schema=vol.Schema(
                 {
                     vol.Required("sensor_id"): str,
-                    #vol method requires station id to be string
                 }
             ),
+            errors=errors,
         )
-        
-
-
 
     async def async_step_sensor_id(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(
-                title = "sensor ID",
-                data = {},
+                title="Sensor ID",
+                data={"sensor_id": user_input["sensor_id"]},
             )
-        
+
         return self.async_show_form(
-            step_id = "sensor_id",
-            data_schema = vol.Schema({
-                vol.Required("sensor_id"):str
-            }),
+            step_id="sensor_id",
+            data_schema=vol.Schema({vol.Required("sensor_id"): str}),
         )
