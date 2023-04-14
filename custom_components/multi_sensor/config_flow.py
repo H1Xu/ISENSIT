b9abaa3d040571818ab0c1a666c039fa98978311
H1Xu <124353124+H1Xu@users.noreply.github.com>
2023-04-14 20:34:25 +0800

    Update config_flow.py

--- custom_components/multi_sensor/config_flow.py
+++ custom_components/multi_sensor/config_flow.py
@@ -1,4 +1,4 @@
-"""Config flow for ISENSIT integration."""
+"""Config flow for ISENSIT integration.
 from homeassistant import config_entries
 from homeassistant.core import callback
 import voluptuous as vol
@@ -22,4 +22,49 @@ class ISENSITFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema(
                 {vol.Required("sensor_id"): str}
             )
+        )"""
+from homeassistant import config_entries
+from homeassistant.core import callback
+
+class ISENSITFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+    ...
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        if user_input is None:
+            return self._show_form(
+                description_placeholders={
+                    "placeholder_text": "Please enter the sensor ID",
+                },
+                errors={},
+                title="ISENSIT Configuration",
+            )
+
+        # Get the sensor ID from the user input
+        sensor_id = user_input["sensor_id"]
+
+        # Store the sensor ID in the options
+        options = {
+            "sensor_id": sensor_id,
+        }
+
+        # Create the config entry and return the options
+        return self.async_create_entry(title="", data=options)
+    
+    def _show_form(self, description_placeholders, errors, title):
+        """Show the form to the user."""
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        "sensor_id",
+                        description={"suggested_value": description_placeholders["placeholder_text"]},
+                    ): str
+                }
+            ),
+            errors=errors,
+            description_placeholders=description_placeholders,
+            title=title,
         )
+
