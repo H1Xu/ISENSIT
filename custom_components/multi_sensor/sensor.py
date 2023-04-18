b0bc17167bba07159d934ef1d0cc2a7bd1f3bb2d
H1Xu <124353124+H1Xu@users.noreply.github.com>
2023-04-18 21:01:52 +0800

    Update sensor.py

--- custom_components/multi_sensor/sensor.py
+++ custom_components/multi_sensor/sensor.py
@@ -1,40 +1,19 @@
 from homeassistant.components.sensor import SensorEntity
 
 
-class CO2Sensor(SensorEntity):
-    """Representation of a CO2 Sensor."""
+class MultiSensorEntity(CoordinatorEntity):
+    """Representation of a MultiSensor entity."""
 
-    def __init__(self, mqtt_client, mqtt_topic, co2_topic):
-        """Initialize the sensor."""
-        self._mqtt_client = mqtt_client
-        self._mqtt_topic = mqtt_topic
-        self._co2_topic = co2_topic
-        self._state = None
+    def __init__(self, coordinator, config_entry, sensor_type):
+        """Initialize the MultiSensor entity."""
+        super().__init__(coordinator)
+        self._config_entry = config_entry
+        self._sensor_type = sensor_type
+        self._attr_extra_state_attributes = {ATTR_SENSOR_TYPE: sensor_type}
 
-    async def async_added_to_hass(self):
-        """Subscribe MQTT events."""
-        await self._mqtt_client.async_subscribe(self._co2_topic, self._async_update)
+    async def async_update(self):
+        """Update the state of the entity."""
+        await super().async_update()
 
-    async def async_will_remove_from_hass(self):
-        """Unsubscribe MQTT events."""
-        await self._mqtt_client.async_unsubscribe(self._co2_topic)
-
-    async def _async_update(self, topic, payload, qos):
-        """Handle MQTT message received."""
-        self._state = int(payload)
-        self.async_write_ha_state()
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return "ISENSIT CO2 Sensor"
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement."""
-        return "ppm"
+        if self._sensor_type == "co2":
+            self._attr_state = self.hass.data[DOMAIN].get("co2")
