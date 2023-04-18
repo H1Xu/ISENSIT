35deffe430792e9ae2a6da9058e35a07187919ab
H1Xu <124353124+H1Xu@users.noreply.github.com>
2023-04-18 17:07:09 +0800

    Update __init__.py

--- custom_components/multi_sensor/__init__.py
+++ custom_components/multi_sensor/__init__.py
@@ -1,82 +1,108 @@
-"""The worlds_air_quality_index component."""
-from __future__ import annotations
-
+"""The multi_sensor component."""
 import logging
+from typing import Any, List, Optional
 
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import (
-    HomeAssistant
-)
-from homeassistant.const import (
-    CONF_LATITUDE, 
-    CONF_LONGITUDE, 
-    CONF_LOCATION,
-    CONF_METHOD,
-    CONF_ID,
-    CONF_TEMPERATURE_UNIT,
-    TEMP_CELSIUS
-)
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
+from homeassistant.const import CONF_NAME, CONF_SENSORS
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import discovery
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType
 
-from .const import (
-    PLATFORMS
+from homeassistant.components.mqtt import (
+    CONF_QOS,
+    CONF_STATE_TOPIC,
+    CONF_AVAILABILITY_TOPIC,
+    MqttAttributes,
+    MqttAvailability,
 )
+from homeassistant.components.mqtt.subscription import (
+    MQTT_SUBSCRIPTION_REGISTRY,
+    async_subscribe_topics,
+)
+from homeassistant.components.mqtt.sensor import PLATFORM_SCHEMA_PREFIX, async_setup_entry_helper
 
 _LOGGER = logging.getLogger(__name__)
 
+SENSOR_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_NAME): cv.string,
+        vol.Required(CONF_STATE_TOPIC): cv.string,
+        vol.Optional(CONF_AVAILABILITY_TOPIC): cv.string,
+        vol.Optional(CONF_QOS, default=0): vol.All(vol.Coerce(int), vol.Range(min=0, max=2)),
+    }
+)
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up World's Air Quality Index from a config entry."""
-
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    entry.async_on_unload(entry.add_update_listener(update_listener))
-    return True
-
-
-async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Handle options update."""
-    await hass.config_entries.async_reload(entry.entry_id)
-
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload worlds_air_quality_index config entry."""
-
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-
-async def async_migrate_entry(hass, config_entry):
-    """Migrate worlds_air_quality_index old entry."""
-    config_entries = hass.config_entries
-    data = config_entry.data
-    version = config_entry.version
-
-    _LOGGER.debug("Migrating World's Air Quality Index entry from version %s", version)
-
-    if version == 1:
-        method = CONF_LOCATION
-        idx = None
-        new_data = {**data, CONF_ID: idx, CONF_METHOD: method}
-
-        latitude = data[CONF_LATITUDE]
-        longitude = data[CONF_LONGITUDE]
-        if latitude is None:
-            latitude = hass.config.latitude
-            new_data = {**new_data, CONF_LATITUDE: latitude}
-        if longitude is None:
-            longitude = hass.config.longitude
-            new_data = {**new_data, CONF_LONGITUDE: longitude}
-        
-        version = 2
-        config_entry.version = version
-        config_entries.async_update_entry(config_entry, data=new_data)
-    
-    if version == 2:
-        tempUnit = TEMP_CELSIUS
-        new_data = {**data, CONF_TEMPERATURE_UNIT: tempUnit}
-        
-        version = 3
-        config_entry.version = version
-        config_entries.async_update_entry(config_entry, data=new_data)
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_SENSORS): cv.schema_with_slug_keys(SENSOR_SCHEMA),
+    },
+    extra=vol.ALLOW_EXTRA,
+)
 
 
-    _LOGGER.info("Migration to version %s successful", version)
+async def async_setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    async_add_entities: AddEntitiesCallback,
+    discovery_info: Optional[Any] = None,
+) -> None:
+    """Set up the multi_sensor platform."""
 
-    return True
+    sensors = [
+        MQTTSensor(
+            name=sensor_cfg[CONF_NAME],
+            state_topic=sensor_cfg[CONF_STATE_TOPIC],
+            availability_topic=sensor_cfg.get(CONF_AVAILABILITY_TOPIC),
+            qos=sensor_cfg[CONF_QOS],
+            unique_id=f"{sensor_cfg[CONF_NAME]}-{sensor_cfg[CONF_STATE_TOPIC]}",
+            icon="mdi:chart-line",
+        )
+        for sensor_cfg in config[CONF_SENSORS].values()
+    ]
+
+    async_add_entities(sensors)
+    async_subscribe_topics(hass, MQTT_SUBSCRIPTION_REGISTRY, [sensor.subscription_topic for sensor in sensors])
+
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up MQTT sensors dynamically through MQTT discovery."""
+
+    setup = await async_setup_entry_helper(hass, config_entry, async_add_entities)
+    if setup:
+        return True
+
+    async def async_discover(discovery_payload):
+        """Discover and add an MQTT sensor."""
+        discovery_klass = sensor_factory(discovery_payload)
+
+        if discovery_klass is None:
+            return
+
+        sensor_name = discovery_payload["name"]
+        state_topic = discovery_payload["state_topic"]
+        availability_topic = discovery_payload.get("availability_topic")
+        qos = discovery_payload.get(CONF_QOS, 0)
+
+        async_add_entities(
+            [
+                discovery_klass(
+                    name=sensor_name,
+                    state_topic=state_topic,
+                    availability_topic=availability_topic,
+                    qos=qos,
+                    unique_id=f"{sensor_name}-{state_topic}",
+                    icon="mdi:chart-line",
+                )
+            ]
+        )
+
+    # Subscribe to MQTT discovery topics
+    await discovery.async_start(
+        hass,
+        "mqtt",
+        async_discover,
+        PLATFORM_SCHEMA_PREFIX
+
