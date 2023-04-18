6435f0b673693695103626ad334b0e7ff7eea896
H1Xu <124353124+H1Xu@users.noreply.github.com>
2023-04-18 16:51:38 +0800

    Update __init__.py

--- custom_components/multi_sensor/__init__.py
+++ custom_components/multi_sensor/__init__.py
@@ -13,7 +13,7 @@ from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, Upda
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = "isensit"
+DOMAIN = "ISENSIT"
 
 CONF_MQTT_BROKER = "broker"
 CONF_MQTT_PORT = "port"
