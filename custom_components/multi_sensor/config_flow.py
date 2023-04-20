d1feb150798438b56e67c6e0715c3ea7f8268b8d
H1Xu <124353124+H1Xu@users.noreply.github.com>
2023-04-20 19:38:15 +0800

    Update config_flow.py

--- custom_components/multi_sensor/config_flow.py
+++ custom_components/multi_sensor/config_flow.py
@@ -1,26 +1,3 @@
-"""Config flow for ISENSIT integration.
-from homeassistant import config_entries
-from homeassistant.core import callback
-import voluptuous as vol
-
-from .const import DOMAIN
-
-
-class ISENSITFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
-
-    VERSION = 1
-
-    async def async_step_user(self, user_input=None):
-        if user_input is not None:
-            # Skip validation
-            return self.async_create_entry(title=user_input["sensor_id"], data=user_input)
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {vol.Required("sensor_id"): str}
-            )
-        )"""
 """Config flow"""
 import logging
 import voluptuous as vol
