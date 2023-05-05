fae837fc0f3286e8c5438a2b834163fbca180c25
H1Xu <124353124+H1Xu@users.noreply.github.com>
2023-05-05 16:17:37 +0800

    Update __init__.py

--- custom_components/multi_sensor/__init__.py
+++ custom_components/multi_sensor/__init__.py
@@ -1,82 +1,39 @@
-"""The worlds_air_quality_index component."""
-from __future__ import annotations
-
+"""The hildebrand_glow_ihd_mqtt component."""
 import logging
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import (
-    HomeAssistant
-)
 from homeassistant.const import (
-    CONF_LATITUDE, 
-    CONF_LONGITUDE, 
-    CONF_LOCATION,
-    CONF_METHOD,
-    CONF_ID,
-    CONF_TEMPERATURE_UNIT,
-    TEMP_CELSIUS
+    CONF_DEVICE_ID,
 )
+from homeassistant.core import HomeAssistant
 
-from .const import (
-    PLATFORMS
-)
+from .const import DOMAIN, CONF_TOPIC_PREFIX
 
 _LOGGER = logging.getLogger(__name__)
 
+PLATFORMS = ["sensor"]
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up World's Air Quality Index from a config entry."""
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Hildebrand Glow IHD MQTT integration."""
 
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    entry.async_on_unload(entry.add_update_listener(update_listener))
-    return True
-
-
-async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Handle options update."""
-    await hass.config_entries.async_reload(entry.entry_id)
-
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload worlds_air_quality_index config entry."""
-
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-
-async def async_migrate_entry(hass, config_entry):
-    """Migrate worlds_air_quality_index old entry."""
-    config_entries = hass.config_entries
-    data = config_entry.data
-    version = config_entry.version
-
-    _LOGGER.debug("Migrating World's Air Quality Index entry from version %s", version)
-
-    if version == 1:
-        method = CONF_LOCATION
-        idx = None
-        new_data = {**data, CONF_ID: idx, CONF_METHOD: method}
-
-        latitude = data[CONF_LATITUDE]
-        longitude = data[CONF_LONGITUDE]
-        if latitude is None:
-            latitude = hass.config.latitude
-            new_data = {**new_data, CONF_LATITUDE: latitude}
-        if longitude is None:
-            longitude = hass.config.longitude
-            new_data = {**new_data, CONF_LONGITUDE: longitude}
-        
-        version = 2
-        config_entry.version = version
-        config_entries.async_update_entry(config_entry, data=new_data)
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = {}
     
-    if version == 2:
-        tempUnit = TEMP_CELSIUS
-        new_data = {**data, CONF_TEMPERATURE_UNIT: tempUnit}
-        
-        version = 3
-        config_entry.version = version
-        config_entries.async_update_entry(config_entry, data=new_data)
-
-
-    _LOGGER.info("Migration to version %s successful", version)
-
     return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    _LOGGER.debug("Setting up Hildebrand Glow IHD MQTT integration")
+
+    if entry.entry_id not in hass.data[DOMAIN]:
+        hass.data[DOMAIN][entry.entry_id] = {}
+
+    hass.data[DOMAIN][entry.entry_id][CONF_DEVICE_ID] = entry.data[CONF_DEVICE_ID].strip().upper().replace(":", "").replace(" ", "")
+    hass.data[DOMAIN][entry.entry_id][CONF_TOPIC_PREFIX] = entry.data.get(CONF_TOPIC_PREFIX, "glow").strip().replace("#", "").replace(" ", "")
+
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, component))
+
+    _LOGGER.debug("Finished setting up Hildebrand Glow IHD MQTT integration")
+    return True
+
