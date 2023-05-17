d274c3ada65ef8cb4e73be04bc283f887b5efda0
H1Xu <124353124+H1Xu@users.noreply.github.com>
2023-05-17 16:52:39 +0800

    Update config_flow.py

--- custom_components/multi_sensor/config_flow.py
+++ custom_components/multi_sensor/config_flow.py
@@ -22,7 +22,7 @@ class HildebrandGlowIHDMQTTConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_show_form(
                 step_id="user", data_schema=vol.Schema({
                     vol.Required(CONF_DEVICE_ID, default='+'):str,
-                    vol.Required(CONF_TOPIC_PREFIX, default='multisensor'):str
+                    vol.Required(CONF_TOPIC_PREFIX, default='/multisensor'):str
                 }), errors=errors
             )
 
@@ -61,6 +61,6 @@ class HildebrandGlowIHDMQTTOptionsFlowHandler(config_entries.OptionsFlow):
 
         data_schema=vol.Schema({
             vol.Required(CONF_DEVICE_ID, default=self.config_entry.options.get(CONF_DEVICE_ID, "+")):str,
-            vol.Required(CONF_TOPIC_PREFIX, default=self.config_entry.options.get(CONF_TOPIC_PREFIX, "multisensor")):str
+            vol.Required(CONF_TOPIC_PREFIX, default=self.config_entry.options.get(CONF_TOPIC_PREFIX, "/multisensor")):str
         })
         return self.async_show_form(step_id="init", data_schema=data_schema)
