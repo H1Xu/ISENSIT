4f7fe12cbfdf7862ea7af1f32f91e6ab8f1a9297
H1Xu <124353124+H1Xu@users.noreply.github.com>
2023-05-31 22:45:14 +0800

    Update button.py

--- custom_components/multi_sensor/button.py
+++ custom_components/multi_sensor/button.py
@@ -6,6 +6,10 @@ import paho.mqtt.client as mqtt
 
 import time
 
+HOST = "192.168.1.223"
+PORT = 1883
+
+
 payload = {"unique_id": "multisensor_alarm_switch",
            "name": "Multisensor alarm switch",
            "state_topic": "/multisensor/MS-IPe0e2e6742eff/peripherals/sound/POST",
@@ -33,8 +37,8 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     if (str(rc) == '0'):
         print("start")
-        client.publish(config_topic, payload, qos=1, retain=True)
-        client.publish(topic, "online", qos=1)
+        client.publish(config_topic, payload, qos=0, retain=True)
+        client.publish(topic, "online", qos=0)
 
 
 client = mqtt.Client()
